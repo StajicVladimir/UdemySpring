7b9bea333f2ff0f915c2ff1ee6f9d1ae891d9770
vladimirstajic <vstajic@gmail.com>
2018-06-16 12:02:19 +0200

    added setter injection

---
+++ src/com/training/CricketCoach.java
@@ -0,0 +1,50 @@
+package com.training;
+
+public class CricketCoach implements Coach{
+	
+	private FortuneService fortuneService;
+	private String email;
+	private String team;
+	
+	
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside constructor");
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		System.out.println("inside set email");
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("inside set Team!");
+		this.team = team;
+	}
+
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method");
+		this.fortuneService = fortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Cricket Coach says: play some Cricket";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		return "Cricket Fortune: " + fortuneService.getFortune();
+	}
+}
---
+++ src/com/training/SetterDemoApp.java
@@ -0,0 +1,23 @@
+package com.training;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		CricketCoach cricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		
+		System.out.println(cricketCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyFortune());
+		System.out.println(cricketCoach.getEmail());
+		System.out.println(cricketCoach.getTeam());
+		context.close();
+	}
+
+}
